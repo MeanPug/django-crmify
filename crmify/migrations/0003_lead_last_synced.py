adf40672ad894ede179f595e4197cea170dc4500
Bobby <steinbach.rj@gmail.com>
2018-02-25 20:38:05 -0500

    remove lead_anchor migration since it is a dynamic foreign key (each client app should set the LEAD_MODEL value before running migrations)

--- crmify/migrations/0003_lead_last_synced.py
+++ crmify/migrations/0003_lead_last_synced.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('crmify', '0002_lead_anchor'),
+        ('crmify', '0001_initial'),
     ]
 
     operations = [
